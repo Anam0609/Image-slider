3b4721d2adb7887b8ecf76e8c12ed2dbac1bee68
Anam0609 <Biancamajikijela95@gmail.com>
2020-09-11 03:34:35 +0200

    Updates

---
+++ precache-manifest.836625d32adc41950276462edbf9c676.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fd2cb76a580c39d29502928cc7e523",
+    "url": "/Image-slider/index.html"
+  },
+  {
+    "revision": "2090e31294c21997b486",
+    "url": "/Image-slider/static/css/2.f83ed9fc.chunk.css"
+  },
+  {
+    "revision": "af5d0cc6bbe8952a4e21",
+    "url": "/Image-slider/static/css/main.c81d7101.chunk.css"
+  },
+  {
+    "revision": "2090e31294c21997b486",
+    "url": "/Image-slider/static/js/2.df4eb6d1.chunk.js"
+  },
+  {
+    "revision": "b694b368ac4bf9700c35a7a4d159ea5f",
+    "url": "/Image-slider/static/js/2.df4eb6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5d0cc6bbe8952a4e21",
+    "url": "/Image-slider/static/js/main.4f0314ca.chunk.js"
+  },
+  {
+    "revision": "86e4c9dd6e473e3506f4",
+    "url": "/Image-slider/static/js/runtime-main.22557ca1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Image-slider/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Image-slider/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Image-slider/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Image-slider/static/media/slick.f97e3bbf.svg"
+  }
+]);
